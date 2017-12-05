2ae8f67369349de539071e9d4664714ec22c4bfc
sh8 <s@sh8.io>
2017-12-05 11:50:51 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # go-videoupload
 Easy video uploader for golang
+
+# Thanks
+This library is inspired by [olahol/go-imageupload](https://github.com/olahol/go-imageupload)
+
+# LICENSE
+This library is under the [MIT License](http://opensource.org/licenses/MIT)
+Copyright © 2017, Shun Iwase
---
+++ videoupload.go
@@ -0,0 +1,61 @@
+// Package go-videoupload is wrapper for uploading video files easily
+package videoupload
+
+import (
+	"bytes"
+	"crypto/sha1"
+	"ioutil"
+)
+
+//
+type Video struct {
+	Filename    string
+	ContentType string
+	Data        []byte
+	Size        int
+}
+
+// Save video file with permission 0600
+func (v *Video) Save(filename string) error {
+	return ioutil.WriteFile(filename, i.Data, 0600)
+}
+
+// Get hash sum for creating unique file name
+func (v *Video) getHashSum() string {
+	return sha1.Sum(v.Data)
+}
+
+// Check if the content type is the one of video
+func okContentType(contentType string) bool {
+	return contentType == "video/mp4" || contentType == "video/mpg" || contentType == "video/mpeg"
+}
+
+// Extruct video struct from http.Request
+func Process(r *http.Request, field string) (*Video, error) {
+	file, info, err := r.FormFile(field)
+
+	if err != nil {
+		return nil, err
+	}
+
+	contentType := info.Header.Get("Content-Type")
+
+	if !okContentType(contentType) {
+		return nil, errors.New(fmt.Sprintf("Wrong content type: %s", contentType))
+	}
+
+	bs, err := ioutil.ReadAll(file)
+
+	if err != nil {
+		return nil, err
+	}
+
+	i := &Video{
+		Filename:    info.Filename,
+		ContentType: contentType,
+		Data:        bs,
+		Size:        len(bs),
+	}
+
+	return i, nil
+}
